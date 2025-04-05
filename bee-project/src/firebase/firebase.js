fb832851cb095d3cb63014cf853eba79fc6fbf9c
CS18701-PC\Dao <bocnoc2@gmail.com>
2025-04-05 13:30:57 +0900

    fix firebase

--- bee-project/src/firebase/firebase.js
+++ bee-project/src/firebase/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,6 +17,11 @@ const firebaseConfig = {
   measurementId: "G-ENLX1D4PEZ"
 };
 
-// Initialize Firebase
+// Khởi tạo Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Khởi tạo Authentication
+export const auth = getAuth(app);
+
+// Khởi tạo Firestore
+export const db = getFirestore(app);
